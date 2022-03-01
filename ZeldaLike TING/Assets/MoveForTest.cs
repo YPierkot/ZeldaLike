b0e82636e29684dcc23ec00d2973ac38031d228f
Aurélien <71558277+Arcaane@users.noreply.github.com>
2022-03-01 15:13:56 +0100

    push to merge

---
+++ ZeldaLike TING/Assets/MoveForTest.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForTest : MonoBehaviour
+{
+    public int speed;
+    public bool canMove;
+    public bool goLeft;
+
+    private void Awake()
+    {
+        canMove = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(GoLeft), 0.1f, 5f);
+        InvokeRepeating(nameof(GoRight), 2.5f, 5f);
+    }
+
+    private void Update()
+    {
+        if (!canMove)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+        else
+        {
+            if (goLeft)
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.left * Time.deltaTime * speed;
+            }
+            else
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.right * Time.deltaTime * speed;
+            }
+        }
+    }
+
+    public void GoLeft()
+    {
+        goLeft = true;
+    }
+
+    public void GoRight()
+    {
+        goLeft = false;
+    }
+    
+    public IEnumerator FreezePos()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(2.5f);
+        canMove = true;
+    }
+}
---
+++ ZeldaLike TING/Assets/Scripts/Cards/RedCardGroundEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCardGroundEffect : MonoBehaviour
+{
+    [SerializeField] private LayerMask Ennemy;
+    
+    public void ActivateRedGroundEffect()
+    {
+        Debug.Log("Fireball Short Range Shoot");
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 5, Ennemy);
+        foreach (var col in colliders)
+        {
+            col.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            col.gameObject.GetComponent<ResetColor>().StartCoroutine(col.gameObject.GetComponent<ResetColor>().ResetObjectColor());
+        }
+        Destroy(gameObject, 0.2f);
+    }
+}
