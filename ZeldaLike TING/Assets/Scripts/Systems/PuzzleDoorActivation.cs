2e71e617fd265709581b414aee9afa0a4270015e
Pierkot <yoann59491@yahoo.fr>
2022-05-17 12:55:04 +0200

    udpate island

---
+++ ZeldaLike TING/Assets/Scripts/Systems/PuzzleDoorActivation.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PuzzleDoorActivation : MonoBehaviour {
+    [SerializeField] private int numberOfToObjectToActivate = 0;
+    [SerializeField] private Transform enemyParent = null;
+    [Space]
+    [SerializeField] private GameObject wall = null;
+    private int activatedObject = 0;
+    private bool checkForParent = false;
+
+    public void ActivateObject() {
+        activatedObject++;
+        checkForParent = activatedObject >= numberOfToObjectToActivate;
+    }
+
+    private void Update() {
+        if (!checkForParent) return;
+        if (enemyParent.childCount > 0) return;
+        Destroy(wall);
+        Destroy(gameObject);
+    }
+}
