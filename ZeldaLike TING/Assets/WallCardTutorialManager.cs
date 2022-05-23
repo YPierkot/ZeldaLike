3dcbce7a22e93d11e04a0f48187d860be412e98c
Sach <sach9130@gmail.com>
2022-05-23 17:42:26 +0200

    fix

--- ZeldaLike TING/Assets/WallCardTutorialManager.cs
+++ ZeldaLike TING/Assets/WallCardTutorialManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class WallCardTutorialManager : MonoBehaviour
 {
@@ -9,34 +11,30 @@ public class WallCardTutorialManager : MonoBehaviour
     
     private Queue<DialogueScriptable> dialogueQueue;
     [SerializeField] private List<DialogueScriptable> dialogues;
-    [SerializeField] private HelpsManager HelpsManager;
+    [SerializeField] private VolumeProfile forestProfile;
+    
     
     [Header("State Bools")]
     
     public bool canStart;
-    private bool lianaSet;
     public bool isFinished;
     private bool spawnedEnemies;
-    private bool canSpawnLianas;
-    
+
     [Header("First Challenge")]
     
+    [SerializeField] private Transform objective;
+
+    [Header("Second Challenge")] 
     
-    [Header("Second Challenge")]
-    
-    
-    [Header("Third Challenge")]
-   
+    [SerializeField] private EnemySpawnTrigger spawner;
     
     [Header("Blocking Player")]
     
     [SerializeField] private GameObject barrier;
-    [SerializeField] private GameObject pasStairBarrier = null;
-    [SerializeField] private GameObject platformBarrier;
-
 
     private void Start()
     {
+        
         dialogueQueue = new Queue<DialogueScriptable>();
         foreach (DialogueScriptable dialogue in dialogues)
         {
@@ -50,27 +48,48 @@ public class WallCardTutorialManager : MonoBehaviour
         if (canStart && !DialogueManager.Instance.isPlayingDialogue)
         {
             PlayerStat.instance.life = PlayerStat.instance.lifeMax;
-            barrier.SetActive(true);
             int remainingDialogue = dialogueQueue.Count;
             switch (remainingDialogue)
             {
-                case 3:
+                case 2:
                     DialogueManager.Instance.AssignDialogue(dialogueQueue.Dequeue().dialogue.ToList());
-                    break;
-                case 2 :
-                    
+                    GameManager.Instance.TutorialWorld();
+                    GameManager.Instance.VolumeTransition(GameManager.Instance.tutorialTransition, GameManager.Instance.cardTutorialCurve);
                     break;
                 case 1 :
+                    if (Vector3.Distance(objective.position, Controller.instance.transform.position) <= 3)
+                    {
+                        DialogueManager.Instance.AssignDialogue(dialogueQueue.Dequeue().dialogue.ToList());
+                    }
                     
                     break;
-                
                 case 0 :
-                    
+                    if (!spawnedEnemies)
+                    {
+                        spawner.SpawnEnemies();
+                        spawnedEnemies = true;
+                        
+                    }
+                    if (spawner.enemiesParent.childCount == 3)
+                    {
+                        GameManager.Instance.volumeManager.profile = forestProfile;
+                        isFinished = true;
+                        canStart = false;
+                        barrier.SetActive(false);
+                        
+                    }
                     break;
-
-                    
             }
             
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isFinished)
+        {
+            canStart = true;
+            barrier.SetActive(true);
         }
     }
 }
