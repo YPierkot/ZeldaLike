9ff1779958b3322aa5ca2d563ee839e1d28340c1
TheoooooM <81494904+TheoooooM@users.noreply.github.com>
2022-05-26 21:41:01 +0200

    Liane  pr Sacha

---
+++ ZeldaLike TING/Assets/Scripts/LD/Editor/vineEditor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Vine))]
+public class vineEditor : InteracteObjectEditor
+{
+   public override void OnInspectorGUI()
+   {
+      base.OnInspectorGUI();
+      EditorGUILayout.PropertyField(serializedObject.FindProperty("lianaBurnMaterial"));
+      EditorGUILayout.PropertyField(serializedObject.FindProperty("burnAmount"));
+   }
+}
